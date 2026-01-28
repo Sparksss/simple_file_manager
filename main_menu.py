a62e75c9809fcd16a37d7792af3dd48ad7e49232
Rogatkin <ilya.rogatkin@octo.uz>
2026-01-28 10:37:51 +0500

    edit text file: add save

--- main_menu.py
+++ main_menu.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 from tkinter import filedialog
 
+current_file_path = None
 
 def open_file_dialog(event):
+    global current_file_path
     file_path = filedialog.askopenfilename(filetypes=[("Text files","*.txt")])
     print(f"Open a file with name: {file_path}")
     if file_path:
         print(f"Selected file: {file_path}")
+        current_file_path = file_path
         try:
             with open(file_path, 'r') as f:
                 content = f.read()
@@ -15,6 +18,20 @@ def open_file_dialog(event):
         except Exception as e:
             print(f"Error reading file: {e}")
 
+
+def save_file(event):
+    global current_file_path
+    if not current_file_path:
+        current_file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
+
+    if current_file_path:
+        try:
+            content = text_widget.get("1.0", "end-1c")
+            with open(current_file_path, 'w') as f:
+                f.write(content)
+            print(f"Saved file: {current_file_path}")
+        except Exception as e:
+            print(f"Saved file: {current_file_path}")
 
 
 def clean_area(event):
@@ -31,6 +48,11 @@ btn = tk.Button(root, bg="blue", fg="black")
 btn['text'] = "Open file"
 btn.bind("<Button-1>", open_file_dialog)
 btn.pack()
+
+save_btn = tk.Button(root, bg="green", fg="white")
+save_btn['text'] = "Save"
+save_btn.bind("<Button-1>", save_file)
+save_btn.pack()
 
 clean_btn = tk.Button(root, bg="lightblue", fg="black")
 clean_btn['text'] = "Clean"
